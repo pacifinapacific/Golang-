9227f17c9188fe8f5ae2ef896371976d0fc02ddf
pacifinapacific <34758924+pacifinapacific@users.noreply.github.com>
2022-07-28 07:14:28 +0000

    CH2コミット~46

---
+++ CH2/convert.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 1234
+	var u uint32 = uint32(i)
+
+	var f float32 = float32(u)
+	var s string = string(i) // Unicodeコードポイントに対応した文字列になる
+	var b []byte = []byte("abc")
+
+	fmt.Println(u)
+	fmt.Println(f)
+	fmt.Println(s)
+	fmt.Println(b)
+}
